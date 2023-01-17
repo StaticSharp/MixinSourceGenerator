60eb67a5a9e3be8ca1232c6d7d050cb1bf9493e3
Pavel Zybenkov <pavel_zybenkov@mail.ru>
2023-01-17 21:38:05 +0300

    Prepared to publish to nuget

--- Launcher/Program.cs
+++ Launcher/Program.cs
@@ -1,13 +1,9 @@
-﻿using RoslynSourceGeneratorLauncher;
-using Microsoft.Build.Locator;
-using Microsoft.CodeAnalysis.MSBuild;
-using MixinSourceGenerator;
-
-public class Program
+﻿public class Program
 {
     public static async Task Main()
     {
         var targetProjectPath = Path.Combine(ProjectDirectory.Path, "..\\TestProject\\MixinSourceGenerator.TestProject.csproj");
+        //var targetProjectPath = Path.Combine(ProjectDirectory.Path, "..\\..\\StaticSharp\\StaticSharp\\StaticSharp.csproj");
         var outputPath = Path.Combine(Path.GetDirectoryName(targetProjectPath), $".generated/{typeof(MixinSourceGenerator.MixinSourceGenerator).FullName}");
         await RoslynSourceGeneratorLauncher.RoslynSourceGeneratorLauncher.Launch(new MixinSourceGenerator.MixinSourceGenerator(), targetProjectPath, outputPath);
     }
--- TestProject/TargetClass.cs
+++ TestProject/TargetClass.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MixinSourceGenerator;
-
-namespace MixinSourceGeneratorTestProject
+﻿namespace MixinSourceGeneratorTestProject
 {
     [Mix(typeof(MixinClass))]
     public partial class TargetClass
